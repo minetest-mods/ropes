e2eb4c6db3f54b9247e583724883f0338de9f7c3
FaceDeer <derksenmobile@gmail.com>
2017-01-29 11:41:02 -0700

    add settings for rope lengths

--- init.lua
+++ init.lua
@@ -1,8 +1,18 @@
 vines = {
   name = 'vines',
-  ropeLadderLength = 50,
-  ropeLength = 50,
 }
+
+local rope_length = minetest.setting_get("vines_rope_length")
+if not rope_length then
+	rope_length = 50
+end
+vines.ropeLength = rope_length
+
+local rope_ladder_length = minetest.setting_get("vines_rope_ladder_length")
+if not rope_ladder_length then
+	rope_ladder_length = 50
+end
+vines.ropeLadderLength = rope_ladder_length
 
 dofile( minetest.get_modpath( vines.name ) .. "/functions.lua" )
 dofile( minetest.get_modpath( vines.name ) .. "/aliases.lua" )
