289a3c4c2d0a75f0d68d4022fac96888d9ed03fe
FaceDeer <derksenmobile@gmail.com>
2017-01-31 20:32:40 -0700

    add back in vines crafting compatibility, add a screenshot and description.txt

--- crafts.lua
+++ crafts.lua
@@ -15,15 +15,15 @@ minetest.register_craftitem("ropes:ropesegment", {
 	description = "Rope",
 	_doc_items_longdesc = ropes.doc.ropesegment_longdesc,
     _doc_items_usagehelp = ropes.doc.ropesegment_usage,
-	groups = {ropes = 1},
+	groups = {vines = 1},
 	inventory_image = "ropes_item.png",
 })
 
 minetest.register_craft({
 	output = "ropes:ropeladder_top",
 	recipe =  {
-		{'group:ropes','default:stick','group:ropes'},
-		{'group:ropes','default:stick','group:ropes'},
+		{'group:vines','default:stick','group:vines'},
+		{'group:vines','default:stick','group:vines'},
 	}
 })
 
@@ -31,7 +31,7 @@ minetest.register_craft({
 	output = "ropes:1rope_block",
 	recipe =  {
 		{'default:wood',},
-		{'group:ropes',},
-		{'group:ropes'}
+		{'group:vines',},
+		{'group:vines'}
 	}
 })
