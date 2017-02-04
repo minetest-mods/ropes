0be6c8d95ace8cb3887e4d00f2fab5fbe0286447
FaceDeer <derksenmobile@gmail.com>
2017-02-04 00:00:25 -0700

    make everything flammable

--- crafts.lua
+++ crafts.lua
@@ -37,3 +37,52 @@ minetest.register_craft({
 		{'group:vines'}
 	}
 })
+
+local cotton_burn_time = 3
+local wood_burn_time = minetest.get_craft_result({method="fuel", width=1, items={ItemStack("default:wood")}}).time
+local rope_burn_time = cotton_burn_time * 3
+local rope_box_burn_time = wood_burn_time + rope_burn_time * 2
+local stick_burn_time = minetest.get_craft_result({method="fuel", width=1, items={ItemStack("default:stick")}}).time
+local ladder_burn_time = rope_burn_time * 2 + stick_burn_time * 2
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "ropes:ropesegment",
+	burntime = rope_burn_time,
+})
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "ropes:1rope_block",
+	burntime = rope_box_burn_time,
+})
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "ropes:2rope_block",
+	burntime = rope_box_burn_time * 2,
+})
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "ropes:3rope_block",
+	burntime = rope_box_burn_time * 3,
+})
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "ropes:4rope_block",
+	burntime = rope_box_burn_time * 4,
+})
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "ropes:5rope_block",
+	burntime = rope_box_burn_time * 5,
+})
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "ropes:ropeladder_top",
+	burntime = ladder_burn_time,
+})
